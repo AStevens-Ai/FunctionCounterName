2825ed60a325014d1b730e802d9637a6a235c0f4
Alexander Stevens <murakumo@Alexanders-MacBook-Pro.local>
2023-10-03 15:15:49 -0400

    Second Commit

--- src/CounterName.jsx
+++ src/CounterName.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export function CounterName() {
     const [name, setName] = useState("")
     const [age, setAge] = useState(0)
 
+    useEffect(() => {
+        console.log("Age and name Changed", age, name)
+    }, [age, name]
+)
 
     return( <div>
         <input value={name} onChange={e => setName(e.target.value)}/>
@@ -12,7 +16,8 @@ export function CounterName() {
         <button onClick={() => setAge(currentAge => currentAge - 1)}>-</button>
         {age}
         <button onClick={() => setAge(currentAge => currentAge + 1)}>+</button>
-        
+        <br />
+        <h1>Hello, I am {name} and i am {age} years old.</h1>
     </div>
     )
 }
